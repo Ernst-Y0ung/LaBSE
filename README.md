f152008a70228dd87bdfec7922b456ba11e89a1c
Astroneko404 <33112694+Astroneko404@users.noreply.github.com>
2021-02-08 11:05:20 +0800

    package

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Please download the LaBSE model and put it here
+简单的打了个包，模型请自行前往[https://arxiv.org/pdf/2007.01852.pdf](https://arxiv.org/pdf/2007.01852.pdf)下载
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='labse-tools',
+      version="0.1",
+      description="Test for pip install git+",
+      url="https://github.com/Ernst-Y0ung/LaBSE",
+      install_requires=['numpy', 'tensorflow>=2.0', 'tensorflow_hub', 'bert-for-tf2'],
+      packages=find_packages(),
+      )
