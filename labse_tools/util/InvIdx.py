b8a48376eacd490717f49674233b6dd3af9c5b48
Astroneko404 <33112694+Astroneko404@users.noreply.github.com>
2021-02-25 10:22:55 +0800

    add_inv_idx

--- labse_tools/util/InvIdx.py
+++ labse_tools/util/InvIdx.py
@@ -3,7 +3,7 @@ import pandas as pd
 from zhon.hanzi import punctuation
 
 
-def Tokenizer(corpus_raw, stopwords):
+def tokenize(corpus_raw, stopwords):
     """
     Tokenize the corpus list
     :param corpus_raw: List of texts that are not preprocessed
@@ -22,5 +22,31 @@ def Tokenizer(corpus_raw, stopwords):
     return corpus
 
 
+def build_invidx(corpus):
+    """
+    Build the inverted index of the corpus
+    :param corpus: List of tokenized corpus
+    :return: inv_idx: The inverted index; inv_idx_count: Counter
+    """
+    inv_idx = {}
+    inv_idx_count = {}
+
+    for i in range(len(corpus)):
+        for word in corpus[i]:
+            if word not in inv_idx:
+                inv_idx[word] = set()
+            inv_idx[word].add(i)
+
+    for key, value in inv_idx.items():
+        inv_idx_count[key] = len(value)
+    inv_idx_count = dict(sorted(inv_idx_count.items(), reverse=True, key=lambda item: item[1]))
+
+    return inv_idx, inv_idx_count
+
 def hasNumbers(inputString):
+    """
+    Helper function that checks if a string contains at least one digit
+    :param inputString: String to check
+    :return: True if the string contains at least one digit, otherwise False
+    """
     return any(char.isdigit() for char in inputString)
