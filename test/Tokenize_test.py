6404210a9358068b85d308facb2a50128eb639ad
Astroneko404 <33112694+Astroneko404@users.noreply.github.com>
2021-03-15 14:56:03 +0800

    update_indexing

--- test/Tokenize_test.py
+++ test/Tokenize_test.py
@@ -1,11 +1,11 @@
 import sys
 sys.path.append("../")
-from labse_tools.util import InvIdx
+from labse_tools.util import indexing
 
 
 if __name__ == '__main__':
     stopwords = open('data/baidu_stopwords.txt', 'r', encoding='utf-8').read().split()
     sentence = ['尽管年初监管层连出政策组合拳，1月人民币兑美元仍升值1.22%录得连八月涨势，人民币CFETS指数也一路攀升至逾两年半新高。']
     flag_need = ['n', 'nr', 'ns', 'nt', 'nw', 'nz', 'PER', 'LOC', 'ORG']
-    tokens = InvIdx.tokenize(sentence, stopwords, flag_need)
+    tokens = indexing.tokenize(sentence, stopwords, flag_need)
     print(tokens)
