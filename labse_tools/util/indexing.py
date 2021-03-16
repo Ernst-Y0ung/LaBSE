c9c393f1f97d141978cac90d0c57e711bb06eb0a
Astroneko404 <33112694+Astroneko404@users.noreply.github.com>
2021-03-16 10:29:32 +0800

    update_indexing

--- labse_tools/util/indexing.py
+++ labse_tools/util/indexing.py
@@ -1,61 +1,61 @@
-def build_inv_index(corpus):
-    """
-    Build the inverted index of the corpus
-    :param corpus: List of tokenized corpus
-    :return: inv_idx: The inverted index; inv_idx_count: Counter
-    """
-    inv_idx = {}
-    inv_idx_count = {}
+class Indexing:
+    def __init__(self, corpus):
+        self.corpus = corpus
+        self.adj_mat = []
+        self.mapper_inv = []
+        self.inv_idx = {}
+        self.inv_idx_count = {}
 
-    for i in range(len(corpus)):
-        for word in corpus[i]:
-            if word not in inv_idx:
-                inv_idx[word] = set()
-            inv_idx[word].add(i)
+    def build_inv_index(self):
+        """
+        Build the inverted index of the corpus
+        """
+        for i in range(len(self.corpus)):
+            for word in self.corpus[i]:
+                if word not in self.inv_idx:
+                    self.inv_idx[word] = set()
+                self.inv_idx[word].add(i)
 
-    for key, value in inv_idx.items():
-        inv_idx_count[key] = len(value)
-    inv_idx_count = dict(sorted(inv_idx_count.items(), reverse=True, key=lambda item: item[1]))
+        for key, value in self.inv_idx.items():
+            self.inv_idx_count[key] = len(value)
+        self.inv_idx_count = dict(sorted(self.inv_idx_count.items(), reverse=True, key=lambda item: item[1]))
+        return
 
-    return inv_idx, inv_idx_count
+    def build_adjacency_matrix(self):
+        """
+        Build the adjacency matrix based on corpus given
+        :return: An adjacency matrix and a mapper containing every keyword and its numeric index
+        """
+        if len(self.inv_idx) == 0:
+            print('Please run build_inv_index first')
+            return
+        count = len(self.inv_idx)
+        mapper = {}
+        self.adj_mat = [[0 for _ in range(count)] for _ in range(count)]
 
-def build_adjacency_matrix(corpus, inv_idx):
-    """
-    Build the adjacency matrix based on corpus given
-    :param corpus: A list containing list of keywords (for each articles)
-    :param inv_idx: The inverted index of the corpus
-    :return: An adjacency matrix and a mapper containing every keyword and its numeric index
-    """
-    count = len(inv_idx)
-    mapper = {}
-    mapper_inv = []
-    adj_mat = [[0 for _ in range(count)] for _ in range(count)]
+        for idx, (item, value) in enumerate(self.inv_idx.items()):
+            mapper[item] = idx
+            self.mapper_inv.append(item)
 
-    for idx, (item, value) in enumerate(inv_idx.items()):
-        mapper[item] = idx
-        mapper_inv.append(item)
+        for idx, (item, value) in enumerate(self.inv_idx.items()):
+            from_idx = idx
+            for article in value:
+                for token in self.corpus[article]:
+                    to_idx = mapper[token]
+                    self.adj_mat[from_idx][to_idx] += 1
 
-    for idx, (item, value) in enumerate(inv_idx.items()):
-        from_idx = idx
-        for article in value:
-            for token in corpus[article]:
-                to_idx = mapper[token]
-                adj_mat[from_idx][to_idx] += 1
+        assert len(self.adj_mat) == len(self.adj_mat[0])
+        return self.adj_mat, self.mapper_inv
 
-    assert len(adj_mat) == len(adj_mat[0])
-    return adj_mat, mapper_inv
-
-def dump_adjacency_matrix(outfile, adj_mat, mapper_inv):
-    """
-    Dump an adjacency matrix to a file
-    :param outfile: Path of the output file
-    :param adj_mat: The adjacency matrix to be dumped
-    :param mapper_inv: A mapper containing every keyword from adj_mat and its numeric index
-    :return:
-    """
-    with open(outfile, 'a', encoding='utf-8') as file:
-        for idx, line in enumerate(adj_mat):
-            token = mapper_inv[idx]
-            line = '\t'.join([str(e) for e in line])
-            # print(line)
-            file.write(token + '\t' + line + '\n')
+    def dump_adjacency_matrix(self, outfile):
+        """
+        Dump an adjacency matrix to a file
+        :param outfile: Path of the output file
+        :return:
+        """
+        with open(outfile, 'a', encoding='utf-8') as file:
+            for idx, line in enumerate(self.adj_mat):
+                token = self.mapper_inv[idx]
+                line = '\t'.join([str(e) for e in line])
+                # print(line)
+                file.write(token + '\t' + line + '\n')
--- test/Tokenize_test.py
+++ test/Tokenize_test.py
@@ -1,11 +1,23 @@
 import sys
 sys.path.append("../")
-from labse_tools.util import indexing
+from labse_tools.util.indexing import Indexing
+from labse_tools.util.preprocess import tokenize
+import pandas as pd
 
 
 if __name__ == '__main__':
     stopwords = open('data/baidu_stopwords.txt', 'r', encoding='utf-8').read().split()
-    sentence = ['尽管年初监管层连出政策组合拳，1月人民币兑美元仍升值1.22%录得连八月涨势，人民币CFETS指数也一路攀升至逾两年半新高。']
+    # sentence = ['尽管年初监管层连出政策组合拳，1月人民币兑美元仍升值1.22%录得连八月涨势，人民币CFETS指数也一路攀升至逾两年半新高。']
+    df = pd.read_json('E:/Github/similarity visualization/data/Data.json', orient='index')
+    corpus_raw = df['Text'].tolist()
+
+    # Tokenizing
     flag_need = ['n', 'nr', 'ns', 'nt', 'nw', 'nz', 'PER', 'LOC', 'ORG']
-    tokens = indexing.tokenize(sentence, stopwords, flag_need)
-    print(tokens)
+    corpus = tokenize(corpus_raw, stopwords, flag_need)
+
+    # Indexing
+    idx = Indexing(corpus)
+    idx.build_inv_index()
+    idx.build_adjacency_matrix()
+    print(idx.inv_idx)
+    # print(idx.adj_mat)
